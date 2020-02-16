6e42743379228bfdce871a321b83ff46841e153e
Murat D <murat_dindar@yahoo.com>
2020-02-16 07:19:19 +0300

    validateToken method implemented in JWTService.

--- src/main/java/muro/room/booking/service/JWTService.java
+++ src/main/java/muro/room/booking/service/JWTService.java
@@ -3,6 +3,7 @@ package muro.room.booking.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -11,6 +12,7 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Base64;
 import java.util.Date;
 
 @Service
@@ -35,5 +37,14 @@ public class JWTService {
                 .withClaim("role", role)
                 .withExpiresAt(new Date(System.currentTimeMillis() + expTime))
                 .sign(Algorithm.RSA256(publicKey, privateKey));
+    }
+
+    public String validateToken(String token) throws JWTVerificationException {
+        String encodedPayload =  JWT.require(Algorithm.RSA256(publicKey, privateKey))
+                .build()
+                .verify(token)
+                .getPayload();
+
+        return new String(Base64.getDecoder().decode(encodedPayload));
     }
 }
